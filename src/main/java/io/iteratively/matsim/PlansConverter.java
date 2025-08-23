bf06718c9ae6a74577c7736d4957064504df1cef
steffenaxer <26229392+steffenaxer@users.noreply.github.com>
2025-08-23 20:16:38 +0200

    Add start time to export

--- src/main/java/io/iteratively/matsim/PlansConverter.java
+++ src/main/java/io/iteratively/matsim/PlansConverter.java
@@ -155,6 +155,7 @@ public class PlansConverter {
                         double fare = trip.optDouble("fare", 0.0);
                         double tip_share = tip / (tip + fare);
 
+                        leg.getAttributes().putAttribute("trip_start_timestamp", startTime);
                         leg.getAttributes().putAttribute("tip_share", Double.isNaN(tip_share) ? 0.0 : tip_share);
                         leg.getAttributes().putAttribute("additional_charges", trip.optDouble("additional_charges", 0.0));
                         leg.getAttributes().putAttribute("trip_total", trip.optDouble("trip_total", 0.0));
